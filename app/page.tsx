13105ba7401f2b9897ce92ea6a4003314443d7e5
taishikato <taishi.k0903@gmail.com>
2023-04-13 16:16:31 -0700

    add simple LP

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
-import Link from "next/link";
+import Head from "next/head";
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import { Footer } from "./components/Footer";
 
 export default function Home() {
-  return <Link href="/dashboard">Hello</Link>;
+  return (
+    <>
+      <Head>
+        <title>TaxPal - Accounting made simple for small businesses</title>
+        <meta
+          name="description"
+          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        {/* <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Testimonials />
+        <Pricing />
+        <Faqs /> */}
+      </main>
+      <Footer />
+    </>
+  );
 }
