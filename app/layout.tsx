f259a83f33ea02de3fb65bb14ca9ad1d8c84ff5e
taishikato <taishi.k0903@gmail.com>
2023-04-13 13:33:02 -0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "./global.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className="min-h-screen bg-slate-50">{children}</body>
+    </html>
+  );
+}
