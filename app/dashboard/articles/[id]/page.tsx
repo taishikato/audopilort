835a148df09c7f7fac577eedd956c7066e8bb69d
taishikato <taishi.k0903@gmail.com>
2023-04-13 15:45:44 -0700

    create layout component for /articles

---
+++ app/dashboard/articles/[id]/page.tsx
@@ -0,0 +1,13 @@
+export default function Article() {
+  return (
+    <>
+      <main className="lg:pl-72">
+        <div className="xl:pl-96">
+          <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
+            Article details
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ app/dashboard/articles/layout.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+import { DocumentIcon } from "@heroicons/react/24/outline";
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const ArticlesLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      {children}
+      <aside className="fixed inset-y-0 hidden px-3 py-6 overflow-y-auto bg-white border-r border-gray-200 left-72 w-96 sm:px-6 lg:px-8 xl:block">
+        {/* Secondary column (hidden on smaller screens) */}
+        <h3 className="px-3 mb-2 font-semibold leading-6 rounded-md">
+          Articles
+        </h3>
+
+        <ul className="space-y-1">
+          <li>
+            <Link
+              href="/dashboard/articles/123"
+              className={classNames(
+                true
+                  ? "bg-gray-50 text-indigo-600"
+                  : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+              )}
+            >
+              <DocumentIcon
+                className={classNames(
+                  true
+                    ? "text-indigo-600"
+                    : "text-gray-400 group-hover:text-indigo-600",
+                  "h-6 w-6 shrink-0"
+                )}
+                aria-hidden="true"
+              />
+              How to get the latest AI
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/dashboard/articles/456"
+              className={classNames(
+                false
+                  ? "bg-gray-50 text-indigo-600"
+                  : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+              )}
+            >
+              <DocumentIcon
+                className={classNames(
+                  false
+                    ? "text-indigo-600"
+                    : "text-gray-400 group-hover:text-indigo-600",
+                  "h-6 w-6 shrink-0"
+                )}
+                aria-hidden="true"
+              />
+              How to use LangChain
+            </Link>
+          </li>
+        </ul>
+      </aside>
+    </>
+  );
+};
+
+export default ArticlesLayout;
