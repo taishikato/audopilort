2d401f94b9ee445ff09e5a21ff7c86cdc6ac2891
taishikato <taishi.k0903@gmail.com>
2023-04-14 13:38:40 -0700

    add inngest cron

---
+++ pages/api/create-inngest-function.ts
@@ -0,0 +1,18 @@
+/**
+ * This endpoint shouldn't be called by a user.
+ * This is to create an inngest function easily for a developer
+ * @link https://vercel.com/integrations/inngest
+ */
+import { Inngest } from "inngest";
+import { serve } from "inngest/next";
+
+const inngest = new Inngest({ name: "Autopilot" });
+const fn = inngest.createFunction(
+  { name: "Article creation" },
+  { cron: "TZ=America/Los_Angeles 0 8 * * *" },
+  async ({ event }) => {
+    console.log("yo");
+  }
+);
+
+export default serve(inngest, [fn]);
