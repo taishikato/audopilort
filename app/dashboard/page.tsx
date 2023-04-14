82f833e4d219b188bc040834df4688d59c7c0c80
taishikato <taishi.k0903@gmail.com>
2023-04-14 11:04:18 -0700

    save topic

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,38 @@
 "use client";
 
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
 
 export default function Dashboard() {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+  const [topic, setTopic] = useState("");
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { data, error } = await supabase.auth.getUser();
+
+    if (error || data.user === null) return;
+
+    const { data: topicCount, count } = await supabase
+      .from("topics")
+      .select("*", { count: "exact" })
+      .eq("user_id", data.user.id);
+
+    if (count === null) return;
+
+    if (count > 0) {
+      const { data: topicCount, count } = await supabase
+        .from("topics")
+        .update({ content: topic, user_id: data.user.id })
+        .eq("user_id", data.user.id);
+    } else {
+      const { data: topicCount, count } = await supabase
+        .from("topics")
+        .insert({ content: topic, user_id: data.user.id });
+    }
+  };
+
   return (
     <main className="lg:pl-72">
       <div className="">
@@ -28,13 +56,18 @@ export default function Dashboard() {
               >
                 Topics for autopilot to write about
               </label>
-              <form className="flex items-center mt-2 space-x-2">
+              <form
+                className="flex items-center mt-2 space-x-2"
+                onSubmit={(e) => onSubmit(e)}
+              >
                 <input
                   type="text"
                   name="topic"
                   id="topic"
                   className="min-w-[300px] p-2 text-gray-900 border-0 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   placeholder="Latest AI news"
+                  value={topic}
+                  onChange={(e) => setTopic(e.target.value)}
                 />
                 <button
                   type="submit"
