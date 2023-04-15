e359c31f13bbb798eae3b3924a32266ddc38973f
taishikato <taishi.k0903@gmail.com>
2023-04-15 12:37:46 -0700

    update subase edge functions to fetch articles

--- supabase/functions/get-articles/index.ts
+++ supabase/functions/get-articles/index.ts
@@ -1,17 +1,42 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 console.log("Hello from Functions!");
 
 serve(async (req: Request) => {
-  const data = {
-    message: `Hello Taishi!`,
-  };
+  const supabaseClientAdmin = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_SERVICE_KEY") ?? ""
+  );
 
-  return new Response(JSON.stringify(data), {
+  const url = new URL(req.url);
+  const apiKey = url.searchParams.get("api_key");
+
+  if (!apiKey) return new Response("Validation error", { status: 400 });
+
+  const { data: apiKeyData, error: apiKeyError } = await supabaseClientAdmin
+    .from("api_keys")
+    .select("api_key, user_id")
+    .eq("api_key", apiKey);
+
+  if (apiKeyError || apiKeyData.length === 0)
+    return new Response(`Invalid request. ${apiKeyError?.message ?? ""}`, {
+      status: 400,
+    });
+
+  const { data, error } = await supabaseClientAdmin
+    .from("articles")
+    .select("*")
+    .eq("user_id", apiKeyData[0].user_id)
+    .order("created_at", { ascending: false });
+
+  if (error)
+    return new Response(
+      JSON.stringify({ result: "error", detail: error.message }),
+      { headers: { "Content-Type": "application/json" } }
+    );
+
+  return new Response(JSON.stringify(data ?? []), {
     headers: { "Content-Type": "application/json" },
   });
 });
