84658caca366ef02f72b3d1615351378893eb03b
taishikato <taishi.k0903@gmail.com>
2023-04-14 23:21:52 -0700

    update text

--- app/dashboard/articles/page.tsx
+++ app/dashboard/articles/page.tsx
@@ -1,12 +1,9 @@
-"use client";
-
 export default function Articles() {
   return (
     <main className="lg:pl-72">
       <div className="xl:pl-96">
         <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
-          {/* Main area */}
-          Articles
+          No article selected
         </div>
       </div>
     </main>
