32520d704a4160c0f7d6de0cae8a078b05da7720
taishikato <taishi.k0903@gmail.com>
2023-04-15 14:07:17 -0700

    fix button text

--- app/dashboard/articles/[id]/components/Draft.tsx
+++ app/dashboard/articles/[id]/components/Draft.tsx
@@ -38,7 +38,7 @@ export const Draft = ({ content }: { content: string }) => {
             type="submit"
             className="px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Button text
+            Save
           </button>
         </div>
       </form>
